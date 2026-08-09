12478cda44c76a568a13e9609125e0c8449e3f8e
RaisonChen <165262683+RaisonChen@users.noreply.github.com>
2026-08-09 14:08:34 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
-        colorSchemeSeed: Colors.blue,
+        colorSchemeSeed: Colors.green,
       ),
       home: const SingleSwitchPage(),
     );
